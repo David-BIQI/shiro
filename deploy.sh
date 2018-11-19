5ba0fffd65cf89ef28e33711b0bf2d33058256b9
xiebiqi <xiebiqi@51yryc.com>
2018-11-19 19:41:10 +0800

    jenkins的部署文件

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,15 @@
 
 
-# 复制新的工程
-# cp $PROJ_PATH/shiro/target/shiro_test-1.1-SNAPSHOT.jar $TOMCAT_APP_PATH/webapps/
-
-# cd $TOMCAT_APP_PATH/webapps/
-# mv shiro_test-1.1-SNAPSHOT.jar
-
 
 
 # 复制新的工程
-cp $PROJ_PATH/shiro/target/shiro_test-1.1-SNAPSHOT.jar $TOMCAT_APP_PATH/
+# cp $PROJ_PATH/shiro/target/shiro_test-1.1-SNAPSHOT.jar $TOMCAT_APP_PATH/
 
 
-
-# cd $TOMCAT_APP_PATH/webapps/
-# mv shiro_test-1.1-SNAPSHOT.jar ROOT.war
 
 # 启动Tomcat
-cd $TOMCAT_APP_PATH/
-sh java -jar shiro_test-1.1-SNAPSHOT.jar 
+# cd $TOMCAT_APP_PATH/
+# sh nohup  java -jar shiro_test-1.1-SNAPSHOT.jar  & tail -f
 
 
 
