36c9a477eb34ca22f56f53211d562719e03d8006
xiebiqi <xiebiqi@51yryc.com>
2018-11-19 19:59:11 +0800

    jenkins的部署文件

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ mvn install -DskipTests
 ifconfig
 
 # 复制新的工程
-# cp $PROJ_PATH/shiro/target/shiro_test-1.1-SNAPSHOT.jar $TOMCAT_APP_PATH/
+cp $PROJ_PATH/shiro/target/shiro_test-1.1-SNAPSHOT.jar $TOMCAT_APP_PATH/
 
 
 
